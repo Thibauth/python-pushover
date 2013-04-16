7adcac4cf9e86fed4aa9f91631544d7e5bce48a7
Thibaut Horel <thibaut.horel@gmail.com>
2013-04-16 23:39:59 +0200

    Use flask-sphinx-theme for the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -90,15 +90,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'flask_small'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {"index_logo": '',
+                      "index_logo_height": '0px',
+                      "github_fork": '' }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes/flask-sphinx-themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
